c2de6568b0581a6e0f937fbe4ab0054ac3947ec5
Rana Dutta <duttarana189@gmail.com>
2025-02-03 22:55:06 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 
-import connectDB from './config/mongodb.js';
-import authRouter from './routes/authRoutes.js';
-import userRouter from './routes/userRoutes.js';
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,18 +13,30 @@ connectDB();
 
 const allowedOrigins = [
   process.env.FRONTEND_URL || "https://full-stack-authentication.vercel.app",
+  "http://localhost:5173",
 ];
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors({origin: allowedOrigins,credentials:true}));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Api Working');
+app.get("/", (req, res) => {
+  res.send("Api Working");
 });
 
-app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
