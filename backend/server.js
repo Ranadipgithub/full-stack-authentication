bdabd411f7ae038f64deb802a0093f0f9c4b43cd
Rana Dutta <duttarana189@gmail.com>
2025-02-03 14:59:32 +0530

    updated frontend url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = [
+  process.env.FRONTEND_URL || "http://localhost:5173",
+];
 
 app.use(express.json());
 app.use(cors({origin: allowedOrigins,credentials:true}));
